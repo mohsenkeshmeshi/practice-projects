4a7bac39957902b899cb34527bf095e925860866
mohsenkeshmeshi <moh3n.k90@gmail.com>
2026-02-14 08:54:01 +0330

    commit the first project

---
+++ t1/main/main.java
@@ -0,0 +1,17 @@
+package main;
+import java.lang.*;
+public class main {
+	public static void main(String[] args)
+	{
+		double price = 6000.0;
+		int amount = 25;
+		String articleCode = "A-001";
+		boolean status = true;
+		
+		System.out.println("Price = "+price);
+		System.out.println("Amount = "+amount);
+		System.out.println("ArticleCode = "+articleCode);
+		System.out.println("Status = "+status);
+		
+	}
+}
